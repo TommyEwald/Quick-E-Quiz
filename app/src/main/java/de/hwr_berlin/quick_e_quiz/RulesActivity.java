1c8c4f2cc467458ee3b542ca2bd3e54b9d6cea58
Tommy90 <ajsdakj@hdhd.de>
2016-03-17 13:57:49 +0100

    RulesActivity angelegt und in MainActivity integriert, sowie Grundgestaltung :beer:

---
+++ app/src/main/java/de/hwr_berlin/quick_e_quiz/RulesActivity.java
@@ -0,0 +1,16 @@
+package de.hwr_berlin.quick_e_quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.regeln);
+    }
+}
