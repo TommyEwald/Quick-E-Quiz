0d4ab90b9c1c21b1a7bf41032af08a2e1cbb3f50
Tommy90 <ajsdakj@hdhd.de>
2016-03-17 12:45:47 +0100

    neue HighscoreActivity mit HighscoreButton von MainActivity verknüpft :lollipop:

---
+++ app/src/main/java/de/hwr_berlin/quick_e_quiz/HighscoreActivity.java
@@ -0,0 +1,13 @@
+package de.hwr_berlin.quick_e_quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
