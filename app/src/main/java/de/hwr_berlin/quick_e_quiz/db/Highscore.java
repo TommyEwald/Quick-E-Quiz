4a1e3cfcd852e8c6f391688d0382c68fe1fc2a57
Pascal Helbig <pascalhelbig@gmail.com>
2016-04-01 22:24:56 +0200

    Highscore in DB angelegt :dizzy_face:

---
+++ app/src/main/java/de/hwr_berlin/quick_e_quiz/db/Highscore.java
@@ -0,0 +1,16 @@
+package de.hwr_berlin.quick_e_quiz.db;
+
+import com.orm.SugarRecord;
+
+public class Highscore extends SugarRecord {
+    String name;
+    int score;
+
+    public Highscore() {
+    }
+
+    public Highscore(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
