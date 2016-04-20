047d99c06c791e61910926fef03b4f5cfb933887
Pascal Helbig <pascalhelbig@gmail.com>
2016-04-20 14:10:57 +0200

    unnötige Imports entfernt :dragon:

--- app/src/main/java/de/hwr_berlin/quick_e_quiz/HighscoreActivity.java
+++ app/src/main/java/de/hwr_berlin/quick_e_quiz/HighscoreActivity.java
@@ -1,11 +1,9 @@
 package de.hwr_berlin.quick_e_quiz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import java.util.Collections;
 import java.util.List;
 
 import de.hwr_berlin.quick_e_quiz.db.Highscore;
